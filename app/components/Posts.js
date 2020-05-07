89e6ba81d466b82ecae5b01a645b7ba049c8b784
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-05-07 13:41:23 -0700

    add fetchMainPosts functionality to Posts component

--- app/components/Posts.js
+++ app/components/Posts.js
@@ -1,7 +1,60 @@
 import React, { Component } from 'react';
+import { fetchMainPosts } from '../utils/api';
 
 export default class Posts extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			posts: null,
+			error: null,
+			loading: true,
+		};
+	}
+
+	componentDidMount() {
+		this.handleFetch();
+	}
+
+	componentDidUpdate(prevProps) {
+		if (prevProps.type !== this.props.type) {
+			this.handleFetch();
+		}
+	}
+	handleFetch() {
+		this.setState({
+			posts: null,
+			error: null,
+			loading: true,
+		});
+
+		fetchMainPosts(this.props.type)
+			.then(posts =>
+				this.setState({
+					posts,
+					loading: false,
+					error: null,
+				}),
+			)
+			.catch(({ message }) =>
+				this.setState({
+					error: message,
+					loading: false,
+				}),
+			);
+	}
+
 	render() {
+		const { posts, error, loading } = this.state;
+
+		if (loading === true) {
+			return <p>Loading</p>; //!make component
+		}
+
+		if (error) {
+			return <p className='center-text error'>{error}</p>; //?maybe make component
+		}
+		console.log(posts);
 		return <div>Posts</div>;
 	}
 }
