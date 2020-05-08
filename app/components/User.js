28e7161eebd50da14f2317e9a5a44a6c3471fc38
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-05-07 18:36:42 -0700

    add user component

--- app/components/User.js
+++ app/components/User.js
@@ -1,8 +1,79 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import queryString from 'query-string';
+import { fetchUser, fetchPosts } from '../utils/api';
+import { formatDate } from '../utils/helpers';
+import PostsList from './PostsList';
 
 export default class User extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			user: null,
+			loadingUser: true,
+			posts: null,
+			loadingPosts: true,
+			error: null,
+		};
+	}
+
+	componentDidMount() {
+		const { id } = queryString.parse(this.props.location.search);
+
+		fetchUser(id)
+			.then(user => {
+				this.setState({
+					user,
+					loadingUser: false,
+				});
+
+				return fetchPosts(user.submitted.slice(0, 30));
+			})
+			.then(posts => {
+				this.setState({
+					posts,
+					loadingPosts: false,
+				});
+			})
+			.catch(({ message }) => {
+				this.setState({
+					error: message,
+					loadingPosts: false,
+					loadingUser: false,
+				});
+			});
+	}
+
 	render() {
-		return <div>User</div>;
+		const { user, posts, loadingUser, loadingPosts, error } = this.state;
+
+		if (error) {
+			return <p>{error}</p>;
+		}
+
+		return (
+			<>
+				{loadingUser === true ? (
+					<p>Fetching User</p>
+				) : (
+					<>
+						<h1 className='header'>{user.id}</h1>
+						<div className='meta-info-light'>
+							<span>joined {formatDate(user.created)}</span>
+							<span>has {user.karma.toLocaleString()} karma</span>
+						</div>
+						<p dangerouslySetInnerHTML={{ __html: user.about }} />
+					</>
+				)}
+				{loadingPosts === true ? (
+					loadingUser === false && <p>Fetching posts</p>
+				) : (
+					<>
+						<h2>Posts</h2>
+						<PostsList posts={posts} />
+					</>
+				)}
+			</>
+		);
 	}
 }
